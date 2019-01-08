d2d52505e4ad9732ed8a5064fc3fafc3329bc958
hossainemruz <emruz@appscode.com>
2019-01-08 12:40:41 +0600

    added all

--- vendor/github.com/kubedb/apimachinery/apis/catalog/v1alpha1/mongodb_version_helpers.go
+++ vendor/github.com/kubedb/apimachinery/apis/catalog/v1alpha1/mongodb_version_helpers.go
@@ -31,7 +31,7 @@ func (p MongoDBVersion) CustomResourceDefinition() *apiextensions.CustomResource
 		Singular:      ResourceSingularMongoDBVersion,
 		Kind:          ResourceKindMongoDBVersion,
 		ShortNames:    []string{ResourceCodeMongoDBVersion},
-		Categories:    []string{"datastore", "kubedb", "appscode"},
+		Categories:    []string{"datastore", "kubedb", "appscode", "all"},
 		ResourceScope: string(apiextensions.ClusterScoped),
 		Versions: []apiextensions.CustomResourceDefinitionVersion{
 			{
